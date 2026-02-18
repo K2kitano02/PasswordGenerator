f621615dde41757587e0214c8d50241a6b3ebf6a
K2kitano02 <k.naoki02@icloud.com>
2026-02-18 19:33:22 +0900

    Remove posts feature including model, controller, views, tests, and associated fixtures. Update routes to point to home index.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_15_055102) do
+ActiveRecord::Schema[7.2].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
